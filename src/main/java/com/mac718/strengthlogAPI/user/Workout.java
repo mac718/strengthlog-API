124e832bcdbfae0d6432b5a278ca6bf44dd2ccf2
Mike Coon <mac718@gmail.com>
2023-02-21 08:20:30 -0800

    add openapi documentation generation

--- src/main/java/com/mac718/strengthlogAPI/user/Workout.java
+++ src/main/java/com/mac718/strengthlogAPI/user/Workout.java
@@ -14,9 +14,12 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
